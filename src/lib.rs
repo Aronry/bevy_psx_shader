ab609703e4dbaaa3f970001757341a261a8317c8
keef <aronruutli@gmail.com>
2024-03-06 21:48:18 +0200

    back to neutral

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,7 @@ impl Plugin for PsxPlugin {
 
         load_internal_binary_asset!(app, PSX_DITHER_HANDLE, "psx-dith.png", image_load);
 
-        load_internal_binary_asset!(app, PSX_LUT_HANDLE, "crazylut.png", image_load);
+        load_internal_binary_asset!(app, PSX_LUT_HANDLE, "neutral.png", image_load);
 
 
         load_internal_asset!(
