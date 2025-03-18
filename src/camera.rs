3f9af86e073dc8a48635ce6937631f6d4b68986c
Evil Corp <smokedsoftware@gmail.com>
2025-03-18 17:35:30 +0200

    no tonemapping test

--- src/camera.rs
+++ src/camera.rs
@@ -167,7 +167,8 @@ pub fn setup_camera(
             // The camera we are actually rendering to
             let camera = if pixel_camera.hdr {
                 Camera3dBundle {
-                    tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::TonyMcMapface,
+                //    tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::TonyMcMapface,
+                    tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::None,
                     camera: Camera {
                         target: RenderTarget::Image(image_handle.clone()),
                         clear_color: ClearColorConfig::Custom(Color::rgba(0.,0.,0.,0.)),
@@ -193,7 +194,8 @@ pub fn setup_camera(
                 }
             } else {
                 Camera3dBundle {
-                    tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::TonyMcMapface,
+                //    tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::TonyMcMapface,
+                    tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::None,
                     camera: Camera {
                         target: RenderTarget::Image(image_handle.clone()),
                         clear_color: ClearColorConfig::Custom(Color::rgba(0.,0.,0.,0.)),
